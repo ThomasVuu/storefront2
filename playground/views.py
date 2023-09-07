d1ad027b4c9d8eeb2fa6f286c334d7c19df9bab6
Thomas Vu <thomas.vu@hotmail.com>
2023-09-07 20:54:23 +0200

    sending emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,11 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
+    try:
+        # send_mail('subject', 'message', 'thomas.vu@hotmail.com', ['bob@ttcvu.com'])
+        mail_admins('subject', 'message', html_message='message')
+    except BadHeaderError:
+        print("hei")
     return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -182,3 +182,7 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'from@ttcvu.com'
+
+ADMINS = [
+    ('Mosh', 'admin@ttcvu.com')
+]
