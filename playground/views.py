6b9b4f962a459fe96fba0f140df5056e849bcaff
Thomas Vu <thomas.vu@hotmail.com>
2023-09-07 20:57:00 +0200

    attaching files

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,14 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
     try:
         # send_mail('subject', 'message', 'thomas.vu@hotmail.com', ['bob@ttcvu.com'])
-        mail_admins('subject', 'message', html_message='message')
+        # mail_admins('subject', 'message', html_message='message')
+        message = EmailMessage('subject', 'message', 'from@ttcvu.com', ['to@ttcvu.com'])
+        message.attach_file('playground/static/images/dog.jpg')
+        message.send()
     except BadHeaderError:
         print("hei")
     return render(request, 'hello.html', {'name': 'Mosh'})
