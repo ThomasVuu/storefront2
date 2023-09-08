81834b94bf30d704674033c55c922e06e32aad92
Thomas Vu <thomas.vu@hotmail.com>
2023-09-08 16:54:54 +0200

    Creating and executing tasks

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,6 @@
-from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
 
 def say_hello(request):
-    try:
-        # send_mail('subject', 'message', 'thomas.vu@hotmail.com', ['bob@ttcvu.com'])
-        # mail_admins('subject', 'message', html_message='message')
-        # message = EmailMessage('subject', 'message', 'from@ttcvu.com', ['to@ttcvu.com'])
-        # message.attach_file('playground/static/images/dog.jpg')
-        # message.send()
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Mosh'}
-        )
-        message.send(['john@ttcvu.com'])
-    except BadHeaderError:
-        print("hei")
+    notify_customers.delay("Hello")
     return render(request, 'hello.html', {'name': 'Mosh'})
