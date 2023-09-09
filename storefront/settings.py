daccc6c762e72b661767eaa907cb30e576bcd135
Thomas Vu <thomas.vu@hotmail.com>
2023-09-09 13:55:52 +0200

    collectiong static assets

--- storefront/settings.py
+++ storefront/settings.py
@@ -151,7 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#python manage.py collectstatic
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
